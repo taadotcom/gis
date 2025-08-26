7174e19b481ce5c6b857e467e5b35a7616a0c9dd
taadotcom <36702203+taadotcom@users.noreply.github.com>
2025-08-26 14:10:35 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
+            publicDirectory: 'public_html', // Add this line
         }),
         vue({
             template: {
@@ -20,6 +21,9 @@ export default defineConfig({
             },
         }),
     ],
+    build: {
+        outDir: 'public_html/build',
+    },
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
